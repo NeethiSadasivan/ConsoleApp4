07667027b76f7ffe11b178121a8c600670052678
NeethiSadasivan <61489793+NeethiSadasivan@users.noreply.github.com>
2021-09-14 16:29:25 +0530

    from git

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp4
         static void Main(string[] args)
         {
             int a = 10;
+            int b=20;
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.Read();
