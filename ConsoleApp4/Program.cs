e95fa2141f05fe3125b8cd670802959ff280494a
NeethiSadasivan <neethinandu1@gmail.com>
2021-09-15 13:07:49 +0530

    errorrr

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp4
         {
             int a = 10;
             int b=20;
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!")
 
             Console.WriteLine("Hello World!");
             Console.Read();
